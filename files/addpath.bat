cffb789592c0af940bd569b9a3e6951d0b407e8b
Jin Lin <jinlin82@qq.com>
2020-03-29 12:05:20 +0800

    add files

---
+++ files/addpath.bat
@@ -0,0 +1,8 @@
+SetEnv -uap PATH %%"%cd%"
+
+@echo off
+for /f "usebackq tokens=2,*" %%A in (`%SystemRoot%\System32\reg.exe query HKCU\Environment /v PATH`) do set my_user_path=%%B
+echo. && echo User Path: && echo --------------------------------------
+for %%a in ("%my_user_path:;=" "%") do echo %%a
+
+pause
---
+++ files/showpath.bat
@@ -0,0 +1,10 @@
+@echo off
+for /f "usebackq tokens=2,*" %%A in (`%SystemRoot%\System32\reg.exe query HKCU\Environment /v PATH`) do set my_user_path=%%B
+echo. && echo User Path: && echo --------------------------------------
+for %%a in ("%my_user_path:;=" "%") do echo %%a
+
+for /f "usebackq tokens=2,*" %%A in (`%SystemRoot%\System32\reg.exe query "HKLM\System\CurrentControlSet\Control\Session Manager\Environment" /v PATH`) do set system_path=%%B
+echo. && echo System Path: && echo --------------------------------------
+for %%a in ("%system_path:;=" "%") do echo %%a
+
+pause
